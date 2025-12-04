bc8aabfdeac0e131eb21851e410f97acda8ab01c
Faisal Memon <faisalmemonuk@yahoo.com>
2025-12-04 22:03:03 +0000

    Default multiplatform project also pulling in the donut model.

---
+++ mp-rk-demo/mp-rk-demo/mp_rk_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mp_rk_demoApp.swift
+//  mp-rk-demo
+//
+//  Created by Faisal Memon on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct mp_rk_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
