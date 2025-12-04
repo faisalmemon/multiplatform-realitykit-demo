0fbaf08a23b05326f38c5ab291b0ffa702f36456
Faisal Memon <faisalmemonuk@yahoo.com>
2025-12-04 22:31:48 +0000

    Add Info.plist, reference this in the project file, and craft a main app scene which conforms to the plist.  That is, for vision os the scene is volumetric.  Needed otherwise we would crash.

--- mp-rk-demo/mp-rk-demo/mp_rk_demoApp.swift
+++ mp-rk-demo/mp-rk-demo/mp_rk_demoApp.swift
@@ -7,11 +7,33 @@
 
 import SwiftUI
 
+import RealityKit // RealityKit has a Scene, so we need to use SwiftUI.Scene from now on
+import RealityKitContent
+
 @main
 struct mp_rk_demoApp: App {
-    var body: some Scene {
+    
+    init() {
+        RealityKitContent.GestureComponent.registerComponent()
+    }
+    
+#if os(visionOS)
+    var body: some SwiftUI.Scene {
+        WindowGroup {
+            ContentView()
+        }
+        // Info.plist dictionary UIWindowSceneSessionRoleApplication
+        // has item UIWindowSceneSessionRoleVolumetricApplication
+        // Therefore if we don't provide a volumetric window the
+        // system will crash.
+        .windowStyle(.volumetric)
+        .defaultSize(width: 2, height: 1.75, depth: 1, in: .meters)
+    }
+#else
+    var body: some SwiftUI.Scene {
         WindowGroup {
             ContentView()
         }
     }
+#endif
 }
